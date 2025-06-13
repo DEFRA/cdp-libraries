1320a0f6bdbc7d20c8a4ac050b1116d61f30df50
Phil Segal <phil@phisec.co.uk>
2025-06-13 11:58:20 +0100

    CORE-450: Create abstracted joi validations library for SSOps & PFE

---
+++ packages/cdp-validation-kit/src/constants/build-memory-validation.js
@@ -0,0 +1,21 @@
+import Joi from 'joi'
+import { ecsCpuToMemoryOptionsMap } from './ecs-cpu-to-memory-options-map.js'
+
+/**
+ *
+ * @returns {Joi.NumberSchema<number>}
+ */
+function buildMemoryValidation() {
+  return Object.keys(ecsCpuToMemoryOptionsMap).reduce((joiNumber, cpu) => {
+    const validMemoryValues = ecsCpuToMemoryOptionsMap[cpu]?.map(
+      ({ value }) => value
+    )
+
+    return joiNumber.when('cpu', {
+      is: Number.parseInt(cpu),
+      then: Joi.number().valid(...validMemoryValues)
+    })
+  }, Joi.number())
+}
+
+export { buildMemoryValidation }
