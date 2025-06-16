68bfd2f51d0eac50553f41469018f51eb40a315a
Phil Segal <phil@phisec.co.uk>
2025-06-16 11:39:20 +0100

    CORE-450: Add tests

--- packages/cdp-validation-kit/src/validations.test.js
+++ packages/cdp-validation-kit/src/validations.test.js
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest'
-import { repositoryNameValidation } from './validations'
+import { environmentValidation, repositoryNameValidation } from './validations'
 
 describe('validations', () => {
   it('validate repository name validation', () => {
@@ -9,8 +9,20 @@ describe('validations', () => {
 
   it('invalid repository name validation with name longer than 32 characters', () => {
     const result = repositoryNameValidation.validate(
-      'invalid-repo-name-because-it-is-way-too-long-yes-please'
+      'invalid-repo-name-because-it-is-way-too-long'
     )
     expect(result.error.message).toBe('32 characters or less')
+  })
+
+  it('validate environment validation', () => {
+    const result = environmentValidation.validate('test')
+    expect(result.error).toBeUndefined()
+  })
+
+  it('invalid environment validation with incorrect environment name', () => {
+    const result = environmentValidation.validate('invalid-env')
+    expect(result.error.message).toBe(
+      '"value" must be one of [management, infra-dev, dev, test, perf-test, ext-test, prod]'
+    )
   })
 })
