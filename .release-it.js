6be8acdbbac9fa9fa88c93057124b64c596925f0
Ben Chidgey <ben@feedmypixel.com>
2025-06-17 09:49:11 +0100

    CORE-450: HArd code name as ${name} variable does not work

--- .release-it.js
+++ .release-it.js
@@ -4,8 +4,6 @@ export default {
     publish: false
   },
   git: {
-    commitMessage: 'Released ${name} ${version}', // eslint-disable-line no-template-curly-in-string
-    tagName: '${name}-${version}', // eslint-disable-line no-template-curly-in-string
     publish: false,
     requireCleanWorkingDir: false
   }
--- packages/cdp-validation-kit/.release-it.js
+++ packages/cdp-validation-kit/.release-it.js
@@ -1,6 +1,8 @@
 export default {
   increment: 'minor',
   git: {
+    commitMessage: 'Released cdp-validation-kit ${version}', // eslint-disable-line no-template-curly-in-string
+    tagName: 'cdp-validation-kit-${version}', // eslint-disable-line no-template-curly-in-string
     requireCleanWorkingDir: false
   }
 }
