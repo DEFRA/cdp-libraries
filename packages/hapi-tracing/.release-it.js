863d6d6594615965b5f77baef4448b11143c4eeb
Ben Chidgey <ben@feedmypixel.com>
2025-06-18 07:30:02 +0100

    CORE-450: incorporate changests

--- packages/hapi-tracing/.release-it.js
+++
@@ -1,8 +0,0 @@
-export default {
-  increment: 'minor',
-  git: {
-    commitMessage: 'Released hapi-tracing ${version}', // eslint-disable-line no-template-curly-in-string
-    tagName: 'hapi-tracing-${version}', // eslint-disable-line no-template-curly-in-string
-    requireCleanWorkingDir: false
-  }
-}
