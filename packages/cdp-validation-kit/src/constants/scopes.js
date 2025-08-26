9adae51f8655f9728b04ce2737048f4883a01790
Phil Segal <phil@phisec.co.uk>
2025-08-26 14:46:50 +0100

    CORE-1156: Add scopes contants

---
+++ packages/cdp-validation-kit/src/constants/scopes.js
@@ -0,0 +1,11 @@
+const scopes = {
+  admin: 'permission:admin',
+  tenant: 'permission:tenant',
+  serviceOwner: 'permission:serviceOwner',
+  externalTest: 'permission:externalTest',
+  breakGlass: 'permission:breakGlass',
+  restrictedTechPython: 'permission:restrictedTechPython',
+  restrictedTechPostgres: 'permission:restrictedTechPostgres'
+}
+
+export { scopes }
